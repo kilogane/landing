87c91f60f91a63aa72bef214cedc42e95e675318
kilogane <67554202+kilogane@users.noreply.github.com>
2021-12-18 15:14:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>OpenLight</title>
+  <meta description="OpenLight">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no">
+  <link href="css/main.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <header class="header">
+    <h1 class="header__title"> OpenLight</h1>
+    <h2 class="header__subtitle"> Retroiluminación LED<br> para el armario</h2>
+    <ul class="list-reset header__list">
+      <li> Se fija en <span>2</span> movimientos</li>
+      <li><span> 3000</span> horas de <br> vida útil</li>
+      <li><span> Apagado / Encendido</span> automático</li>
+      <li><span>Mecanismo confiable</span></li>
+    </ul>
+    <button class="button show-form animatedBtn"> PEDIR</button>
+    <div class="header__complects complects">
+      <div class="complects__bar">
+        <div class="complects__innerBar"></div>
+      </div>
+      <p class="complects__text"> En almacén quedan <span>10</span> kits</p>
+    </div>
+    <div class="header__price price price__wrapper">
+      <div class="price__oldPrice"> 58€</div>
+      <p class="price__text"> precio con descuento</p>
+      <div class="price__newPrice"> 29€</div>
+      <p class="price__text_yellow"> por kit</p>
+    </div>
+    <div class="header__lamps lamps"></div>
+    <div class="header__sale sale">
+      <div class="sale__content">
+        <div class="sale__text"> descuento</div>
+        <div class="sale__percents"> 50%</div>
+      </div>
+    </div>
+  </header>
+  <main>
+    <section class="block1">
+      <h2 class="standardTitle"> ¡Y se hizo la luz!</h2>
+      <div class="video__wrapper">
+        <video preload="auto" autoplay loop muted webkit-playsinline playsinline class="video video_autoPlay">
+          <source src="img/1.mp4" type="video/mp4"> Your browser does not support the video tag.
+        </video>
+      </div>
+      <p class="standardParagraph"><span> OpenLight</span> es una sencilla, duradera y cómoda solución para iluminar
+        el armari. La vida útil
+        de cada lámpara es de<span class="yellowText"> 3000</span> horas.</p>
+      <p class="standardParagraph mb-0"> El encendido y apagado ocurre de forma automática al abrir y cerrar las puertas
+        del armario.</p>
+    </section>
+    <section class="block2">
+      <h2 class="standardTitle"> ¡No más rincones oscuros!</h2>
+      <div class="video__wrapper">
+        <video preload="auto" autoplay loop muted webkit-playsinline playsinline class="video video_autoPlay">
+          <source src="img/2.mp4" type="video/mp4"> Tu navegador no es compatible con el video
+        </video>
+      </div>
+      <h3> Apto para todo tipo de armarios</h3>
+      <div class="video__wrapper">
+        <video preload="auto" autoplay loop muted webkit-playsinline playsinline class="video video_autoPlay">
+          <source src="img/3.mp4" type="video/mp4"> Tu navegador no es compatible con el video
+        </video>
+      </div>
+      <h3> Se instala en dos movimientos </h3>
+      <div class="video__wrapper">
+        <video preload="auto" autoplay loop muted webkit-playsinline playsinline class="video video_autoPlay">
+          <source src="img/4.mp4" type="video/mp4"> Tu navegador no es compatible con el video
+        </video>
+      </div>
+      <h3> Se encienden y apagan automáticamente </h3>
+    </section>
+    <section class="block3">
+      <h2 class="standardTitle"> Solución segura e inteligente para tu hogar</h2><img src="img/patent.jpg" alt>
+    </section>
+    <header class="header block4">
+      <h1 class="header__title"> OpenLight</h1>
+      <h2 class="header__subtitle"> ¡Quédate del lado de la luz!</h2>
+      <ul class="list-reset header__list">
+        <li><span> Materiales seguros</span></li>
+        <li><span> Mecanismo confiable</span></li>
+        <li><span> Instalación fácil</span></li>
+        <li><span> Larga vida útil</span></li>
+      </ul>
+      <div class="timer__wrapper">
+        <div class="timer">
+          <div class="timer__title"> Para que finalice la promoción quedan:</div>
+          <div class="timer__flexWrap">
+            <div class="timer__flexItem">
+              <div class="timer__numbers timer__hour timer__dotted"> 00</div>
+              <p class="timer__description hours"> horas</p>
+            </div>
+            <div class="timer__flexItem">
+              <div class="timer__numbers timer__minutes timer__dotted"> 00</div>
+              <p class="timer__description minutes"> minutos</p>
+            </div>
+            <div class="timer__flexItem">
+              <div class="timer__numbers timer__seconds"> 00</div>
+              <p class="timer__description seconds"> segundos</p>
+            </div>
+          </div>
+          <div class="timer__expires"> ¡El tiempo se ha agotado! ¡Especialmente para ti lo hemos prolongado 30 minutos
+            más!</div>
+        </div>
+      </div>
+      <button class="button show-form animatedBtn"> PEDIR</button>
+      <div class="header__complects complects">
+        <div class="complects__bar">
+          <div class="complects__innerBar"></div>
+        </div>
+        <p class="complects__text"> En almacén quedan <span>10</span> kits</p>
+      </div>
+      <div class="header__price price price__wrapper">
+        <div class="price__oldPrice"> 58€</div>
+        <p class="price__text"> precio con descuento</p>
+        <div class="price__newPrice"> 29€</div>
+        <p class="price__text_yellow"> por kit</p>
+      </div>
+      <div class="header__lamps lamps"></div>
+      <div class="header__sale sale">
+        <div class="sale__content">
+          <div class="sale__text"> descuento</div>
+          <div class="sale__percents"> 50%</div>
+        </div>
+      </div>
+    </header>
+    <section class="block5">
+      <h2 class="standardTitle"> Una solución a muchos problemas</h2>
+      <div class="effect">
+        <div class="effect__before"> Antes </div>
+        <div class="effect__after"> Después</div><img src="img/effect_1.jpg" alt>
+      </div>
+      <h3> Armario</h3>
+      <div class="effect">
+        <div class="effect__before"> Antes </div>
+        <div class="effect__after"> Después</div><img src="img/effect_2.jpg" alt>
+      </div>
+      <h3> Entresuelo</h3>
+      <div class="effect">
+        <div class="effect__before"> Antes </div>
+        <div class="effect__after"> Después</div><img src="img/effect_3.jpg" alt>
+      </div>
+      <h3> Mesa de noche</h3>
+      <div class="effect">
+        <div class="effect__before"> Antes</div>
+        <div class="effect__after"> Después</div><img src="img/effect_4.jpg" alt>
+      </div>
+      <h3> Cajones</h3>
+    </section>
+    <section class="block6">
+      <h2 class="standardTitle"> Qué dicen los usuarios</h2>
+      <div class="reviews__wrapper">
+        <div class="reviews__count"><span class="current-slide">1</span>/<span class="all-slides">5</span></div>
+        <div class="block6__reviews googleReviews">
+          <div class="googleReviews__item">
+            <div class="googleReviews__wrapper">
+              <div class="googleReviews__authorWrapper"><img src="img/ava_1.png" alt
+                  class="googleReviews__authorAvatar">
+                <div class="googleReviews__author"> Ana María</div>
+                <div class="googleReviews__date"> Hace 1 día</div>
+              </div>
+              <div class="googleReviews__textWrapper">
+                <div class="googleReviews__stars"><img src="img/star.svg" alt><img src="img/star.svg" alt><img
+                    src="img/star.svg" alt><img src="img/star.svg" alt><img src="img/star.svg" alt></div>
+                <div class="googleReviews__text"> Iluminé todas las alacenas de mi cocina y ahora no comprendo como pude
+                  vivir sin ellas. ¡Es realmente cómodo!</div>
+                <div class="googleReviews__posted">
+                  <div><img src="img/logo_google_small.svg" alt></div>
+                  <div>
+                    <p> Posted on</p><img src="img/logo_google_blue.svg" alt>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="googleReviews__item">
+            <div class="googleReviews__wrapper">
+              <div class="googleReviews__authorWrapper"><img src="img/ava_2.png" alt
+                  class="googleReviews__authorAvatar">
+                <div class="googleReviews__author"> Pablo</div>
+                <div class="googleReviews__date"> Hace 1 día</div>
+              </div>
+              <div class="googleReviews__textWrapper">
+                <div class="googleReviews__stars"><img src="img/star.svg" alt><img src="img/star.svg" alt><img
+                    src="img/star.svg" alt><img src="img/star.svg" alt><img src="img/star.svg" alt></div>
+                <div class="googleReviews__text"> Son unas mini lámparas increíbles. Tengo un montón de armarios en mi
+                  garage con herramientas, equipo de pesca, camping y solía tenía que buscar todo el día para encontrar
+                  algo. Ahora puedo ver todo muy rápido. ¡Es súper!</div>
+                <div class="googleReviews__posted">
+                  <div><img src="img/logo_google_small.svg" alt></div>
+                  <div>
+                    <p> Posted on</p><img src="img/logo_google_blue.svg" alt>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="googleReviews__item">
+            <div class="googleReviews__wrapper">
+              <div class="googleReviews__authorWrapper"><img src="img/ava_3.png" alt
+                  class="googleReviews__authorAvatar">
+                <div class="googleReviews__author"> Daniela</div>
+                <div class="googleReviews__date"> Hace 2 días</div>
+              </div>
+              <div class="googleReviews__textWrapper">
+                <div class="googleReviews__stars"><img src="img/star.svg" alt><img src="img/star.svg" alt><img
+                    src="img/star.svg" alt><img src="img/star.svg" alt><img src="img/star.svg" alt></div>
+                <div class="googleReviews__text"> Las compré para mi mamá, ella tiene mala vista y a menudo se confunde
+                  cuando saca algo de la estantería. Esto le facilitó la vida. ¡Gracias por una entrega rápida!</div>
+                <div class="googleReviews__posted">
+                  <div><img src="img/logo_google_small.svg" alt></div>
+                  <div>
+                    <p> Posted on</p><img src="img/logo_google_blue.svg" alt>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="googleReviews__item">
+            <div class="googleReviews__wrapper">
+              <div class="googleReviews__authorWrapper"><img src="img/ava_4.png" alt
+                  class="googleReviews__authorAvatar">
+                <div class="googleReviews__author"> Tómas</div>
+                <div class="googleReviews__date"> Hace 3 días</div>
+              </div>
+              <div class="googleReviews__textWrapper">
+                <div class="googleReviews__stars"><img src="img/star.svg" alt><img src="img/star.svg" alt><img
+                    src="img/star.svg" alt><img src="img/star.svg" alt><img src="img/star.svg" alt></div>
+                <div class="googleReviews__text"> En efecto todo es muy increíble. Instalé todo en un par de minutos y
+                  no tuve que romper nada. Estoy muy satisfecho con la compra.</div>
+                <div class="googleReviews__posted">
+                  <div><img src="img/logo_google_small.svg" alt></div>
+                  <div>
+                    <p> Posted on</p><img src="img/logo_google_blue.svg" alt>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="googleReviews__item">
+            <div class="googleReviews__wrapper">
+              <div class="googleReviews__authorWrapper"><img src="img/ava_5.png" alt
+                  class="googleReviews__authorAvatar">
+                <div class="googleReviews__author"> Tamara</div>
+                <div class="googleReviews__date"> Hace 3 días</div>
+              </div>
+              <div class="googleReviews__textWrapper">
+                <div class="googleReviews__stars"><img src="img/star.svg" alt><img src="img/star.svg" alt><img
+                    src="img/star.svg" alt><img src="img/star.svg" alt><img src="img/star.svg" alt></div>
+                <div class="googleReviews__text"> La relación precio-calidad es de primera. Es algo muy útil para la
+                  casa, me alegró de haberlo comprado. Sólo que no calculé bien y debí haber comprado más. Sin embargo,
+                  ya he hecho el segundo pedido y la verdad es que la entrega es muy rápida. </div>
+                <div class="googleReviews__posted">
+                  <div><img src="img/logo_google_small.svg" alt></div>
+                  <div>
+                    <p> Posted on</p><img src="img/logo_google_blue.svg" alt>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <header class="header block7">
+      <h1 class="header__title"> OpenLight</h1>
+      <h2 class="header__subtitle"> Retroiluminación LED<br> para el armario</h2>
+      <div class="timer__wrapper">
+        <div class="timer">
+          <div class="timer__title"> Para que finalice la promoción quedan:</div>
+          <div class="timer__flexWrap">
+            <div class="timer__flexItem">
+              <div class="timer__numbers timer__hour timer__dotted"> 00</div>
+              <p class="timer__description hours"> horas</p>
+            </div>
+            <div class="timer__flexItem">
+              <div class="timer__numbers timer__minutes timer__dotted"> 00</div>
+              <p class="timer__description minutes"> minutos</p>
+            </div>
+            <div class="timer__flexItem">
+              <div class="timer__numbers timer__seconds"> 00</div>
+              <p class="timer__description seconds"> segundos</p>
+            </div>
+          </div>
+          <div class="timer__expires"> ¡El tiempo se ha agotado! ¡Especialmente para ti lo hemos prolongado 30 minutos
+            más!</div>
+        </div>
+      </div>
+      <form id="orderForm" action method="POST" class="form">
+        <label for> Ejemplo: Ana Carolina</label>
+        <input type="text" name="name" required="required" placeholder="Nombre">
+        <label for> Ejemplo: +34961974686</label>
+        <input type="tel" name="phone" required="required" placeholder="Número de teléfono">
+        <button type="submit" class="button animatedBtn"> Pedir</button>
+      </form>
+      <div class="header__complects complects">
+        <div class="complects__bar">
+          <div class="complects__innerBar"></div>
+        </div>
+        <p class="complects__text"> En almacén quedan <span>10</span> kits</p>
+      </div><img src="img/guards.png" alt class="form__guard">
+      <div class="header__price price price__wrapper">
+        <div class="price__oldPrice"> 58€</div>
+        <p class="price__text"> precio con descuento</p>
+        <div class="price__newPrice"> 29€</div>
+        <p class="price__text_yellow"> por kit</p>
+      </div>
+      <div class="header__lamps lamps"></div>
+      <div class="header__sale sale">
+        <div class="sale__content">
+          <div class="sale__text"> descuento</div>
+          <div class="sale__percents"> 50% </div>
+        </div>
+      </div>
+    </header>
+    <footer>
+      <p> Trazard inc. Grenadierstrasse 12, floor 9-16 4058 Basel, Switzerland<br><a href="policy_es.html"
+          target="_blank"> Política de confidencialidad</a></p>
+    </footer>
+  </main>
+  <div class="feedback"><img src="img/i-phone.png" alt></div>
+  <div class="popup-window closed">
+    <div class="close-popup"></div>
+    <form method="POST" action>
+      <label for="name2"> Ejemplo: Ana Carolina</label>
+      <input id="name2" type="text" name="name" placeholder="Nombre" required>
+      <label for="phone2"> Ejemplo: +34961974686</label>
+      <input id="phone2" type="tel" name="phone" placeholder="Número de teléfono" required>
+      <button type="submit"> Pedir</button>
+    </form>
+  </div>
+
+  <script src="js/main.min.js"></script>
+  <script type="text/javascript" src="https://cdn.ldrock.com/additionals.js?geo=ES"></script>
+
+  <script>
+    (function (m, e, t, r, i, k, a) {
+      m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+      m[i].l = 1 * new Date(); k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+    })
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    ym(75060388, "init", {
+      clickmap: true,
+      trackLinks: true,
+      accurateTrackBounce: true,
+      webvisor: true
+    });
+  </script>
+</body>
+
+</html>
